e3f3dbfc790cabf246d51b9a2e59418971623fad
Srikanth <ksreekanthyadav@gmail.com>
2025-01-06 21:52:51 +0400

    Added Screens

---
+++ src/screens/TrackDetailsScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const TrackDetails = () => {
+    return (
+        <View>
+        <Text>TrackDetails Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default TrackDetails;
